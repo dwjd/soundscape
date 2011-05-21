b2610df11650bea346e3490990170217e0ff152a
David Wolf <dpwolf@mac.com>
2011-05-21 15:16:10 -0400

    Added PhoneGap xcode project

---
+++ Classes/soundscapeAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  soundscapeAppDelegate.h
+//  soundscape
+//
+//  Created by David Wolf on 21/05/11.
+//  Copyright dpwolf 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface soundscapeAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  soundscape
+//
+//  Created by David Wolf on 21/05/11.
+//  Copyright dpwolf 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"soundscapeAppDelegate");
+    [pool release];
+    return retVal;
+}
